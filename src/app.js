47c32e399eef4939a79e8a43e1c132552da7845e
aacgn <4augusto@gmail.com>
2018-11-29 18:24:16 -0300

    Added mealRoute

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const index = require('./routes/index');
 const restaurantRoute = require('./routes/restaurantRoute');
+const mealRoute = require('./routes/mealRoute')
 const orderRoute = require('./routes/orderRoute');
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', index);
 app.use('/restaurant', restaurantRoute);
 app.use('/order', orderRoute);
+app.use('/meal', )
 module.exports = app;
