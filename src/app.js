6a738303b89c41c994f4194fefaeac7ebed40a5f
aacgn <aacgn@cin.ufpe.br>
2018-12-04 17:58:46 -0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const router = express.Router();
@@ -6,6 +7,7 @@ const index = require('./routes/index');
 const restaurantRoute = require('./routes/restaurantRoute');
 const mealRoute = require('./routes/mealRoute')
 const orderRoute = require('./routes/orderRoute');
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', index);
