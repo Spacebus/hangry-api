e0d15a34d6ab64ab387101872573a032dfd955c2
eduardosm7 <esm7@cin.ufpe.br>
2018-12-04 18:16:02 -0300

    Update put instead of post

--- src/routes/mealRoute.js
+++ src/routes/mealRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/mealController')
 router.post('/addMeal', controller.addMeal);
-router.post('/:mealId/updateMeal', controller.updateMeal);
-router.post('/updateAllMeals', controller.updateAllMeals);
+router.put('/:mealId/updateMeal', controller.updateMeal);
+router.put('/updateAllMeals', controller.updateAllMeals);
 module.exports = router;
