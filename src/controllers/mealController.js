0cd5d1f7f28ce437cd3091c6bb01a5df10e279ab
aacgn <aacgn@cin.ufpe.br>
2018-12-04 17:50:55 -0300

    Add imageURL attribute

--- src/controllers/mealController.js
+++ src/controllers/mealController.js
@@ -5,13 +5,13 @@ exports.addMeal = (req, res, next) => {
     let name = req.body.name;
     let price = req.body.price;
     let description = req.body.description;
-    let image = req.body.image;
+    let imageURL = req.body.image;
     let multiplier = req.body.multiplier || 1;
     mongo.connect(async function(err){
         if (err) throw err;
         let db = mongo.conn.db('hangry-test');
         let collection = await db.collection('Meal');
-        let Meal = {'restaurantId': restaurantId,'name': name, 'price': price, 'description': description, 'image': image, 'multiplier': multiplier};
+        let Meal = {'restaurantId': restaurantId,'name': name, 'price': price, 'description': description, 'imageURL': imageURL, 'multiplier': multiplier};
         collection.insertOne(Meal);
         res.status(201).send('Refeição adicionada com sucesso!');
     });
--- src/controllers/restaurantController.js
+++ src/controllers/restaurantController.js
@@ -3,14 +3,14 @@ const mongo = require('../database')
 exports.addRestaurant = (req, res, next) => {
     let name = req.body.name;
     let address = req.body.address;
-    let image = req.body.image;
+    let imageURL = req.body.image;
     let phone = req.body.phoen;
     let description = req.body.description;
     mongo.connect(async function(err){
         if (err) throw err;
         let db = mongo.conn.db('hangry-test');
         let collection = await db.collection('Restaurant');
-        let Restaurant = {'name': name, 'address': address, 'image': image ,'phone': phone, 'description': description};
+        let Restaurant = {'name': name, 'address': address, 'imageURL': imageURL ,'phone': phone, 'description': description};
         collection.insertOne(Restaurant);
         res.status(201).send('Restaurante adicionado com sucesso!');
     });
