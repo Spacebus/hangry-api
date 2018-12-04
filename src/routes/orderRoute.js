d5849591ad4f4ebc5ea0a0c1db8b8bbbea9cf946
aacgn <aacgn@cin.ufpe.br>
2018-12-04 17:11:49 -0300

    Fixing orderRoute

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/orderController')
-router.put('/addOrder', controller.addOrder);
-router.put('/addItem', controller.addItemToOrder);
+router.post('/addOrder', controller.addOrder);
+router.post('/addItem', controller.addItemToOrder);
 module.exports = router;
