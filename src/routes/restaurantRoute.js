3a042b2da764a816a6de11ef0e9ac4a87b038439
aacgn <aacgn@cin.ufpe.br>
2018-12-04 19:16:29 -0300

    Add updateRestaurant into restaurantRoute

--- src/routes/restaurantRoute.js
+++ src/routes/restaurantRoute.js
@@ -4,4 +4,5 @@ const controller = require('../controllers/restaurantController')
 router.get('/', controller.getAllRestaurants);
 router.get('/:restaurantId/getMeals', controller.getAllMealsFromTheRestaurant);
 router.post('/addRestaurant', controller.addRestaurant);
+router.put('/:restaurantId/updateRestaurant', controller.updateRestaurant);
 module.exports = router;
