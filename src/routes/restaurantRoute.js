805dd70db6f0c83e1d1aa7899cabf42e7875f0f8
aacgn <aacgn@cin.ufpe.br>
2018-12-04 17:52:17 -0300

    Fix restaurantRoute

--- src/routes/restaurantRoute.js
+++ src/routes/restaurantRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/restaurantController')
 router.get('/', controller.getAllRestaurants);
-router.get('/:restaurantId/getMeals', controller.getALlMealsFromTheRestaurant);
+router.get('/:restaurantId/getMeals', controller.getAllMealsFromTheRestaurant);
 router.post('/addRestaurant', controller.addRestaurant);
 module.exports = router;
