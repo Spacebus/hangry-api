d1e5da65b556e545ebc968d5825cab4c4ed44303
aacgn <aacgn@cin.ufpe.br>
2018-12-04 17:13:57 -0300

    Add multiplier attribute to Meal model

--- src/controllers/mealController.js
+++ src/controllers/mealController.js
@@ -6,11 +6,12 @@ exports.addMeal = (req, res, next) => {
     let price = req.body.price;
     let description = req.body.description;
     let image = req.body.image;
+    let multiplier = req.body.multiplier;
     mongo.connect(async function(err){
         if (err) throw err;
         let db = mongo.conn.db('hangry-test');
         let collection = await db.collection('Meal');
-        let Meal = {'restaurantId': restaurantId,'name': name, 'price': price, 'description': description, 'image': image};
+        let Meal = {'restaurantId': restaurantId,'name': name, 'price': price, 'description': description, 'image': image, 'multiplier': multiplier};
         collection.insertOne(Meal);
         res.status(201).send('Refeição adicionada com sucesso!');
     });
