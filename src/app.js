e307b3983f389dc1ef3740e8da0375c3f89ba6d2
Mateus Ponciano <mmps@cin.ufpe.br>
2018-12-05 01:38:51 +0000

    add session

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var session = require('express-session');
 var cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,6 +8,10 @@ const index = require('./routes/index');
 const restaurantRoute = require('./routes/restaurantRoute');
 const mealRoute = require('./routes/mealRoute')
 const orderRoute = require('./routes/orderRoute');
+var date = Date.now();
+var token = (date/3).toString()
+
+app.use( session({ secret : token, name : 'sessionId', resave : true, saveUninitialized : true}));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
