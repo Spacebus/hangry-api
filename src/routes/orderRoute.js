1d02cd072735f198196233149f15a3c3309b169d
aacgn <4augusto@gmail.com>
2018-12-08 13:09:32 -0300

    Add updateOrder

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/orderController')
 router.post('/', controller.addOrder);
 router.get('/', controller.getAllOrders);
+router.put('/:id', controller.updateOrder);
 router.get('/:id/getAllMealsFromOrder', controller.getAllMealsFromOrder);
 module.exports = router;
